4a61a0d026dee219391731604387351fef6d0668
qudo <59782726+qudo-code@users.noreply.github.com>
2023-04-06 22:34:32 -0700

    docs

--- src/lib/util/solana/connect.ts
+++
@@ -1,8 +0,0 @@
-import { Connection } from "@solana/web3.js";
-
-const { HELIUS_KEY } = process.env;
-
-export default () => new Connection(
-    `https://rpc.helius.xyz/?api-key=${HELIUS_KEY}`,
-    "confirmed"
-);
